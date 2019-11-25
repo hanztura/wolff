a657eedbefdbf8524cd1380918ee91b9ad6a5b7e
hanz <hctura.official@gmail.com>
2019-11-25 22:41:37 +0800

    new contacts app

---
+++ wolff/contacts/models.py
@@ -0,0 +1,25 @@
+from uuid import uuid4
+
+from django.db import models
+
+from system.utils import CompanyFieldMixin
+
+
+class Contact(CompanyFieldMixin, models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    last_name = models.CharField(max_length=50, blank=True)
+    first_name = models.CharField(max_length=50, blank=True)
+    middle_name = models.CharField(max_length=50, blank=True)
+    is_individual = models.BooleanField(default=True, blank=True)
+    registered_name = models.CharField(max_length=200, blank=True)
+
+    @property
+    def name(self):
+        if self.is_individual:
+            name = '{}, {}'.format(self.last_name, self.first_name)
+        else:
+            name = self.registered_name
+        return name
+
+    def __str__(self):
+        return self.name
