1d456ddbf234a51a2ede31af1d03ccaf735e98b7
hanz <hctura.official@gmail.com>
2019-11-26 00:05:06 +0800

    new journal model

--- wolff/accounting/models.py
+++ wolff/accounting/models.py
@@ -1,6 +1,9 @@
 from enum import Enum
+from uuid import uuid4
 
 from django.db import models
+
+from django_extensions.db.models import TimeStampedModel
 
 from system.utils import CompanyFieldMixin, NameAsStrMixin
 
@@ -22,6 +25,28 @@ class Account(CompanyFieldMixin, models.Model):
     account_type = models.ForeignKey(
         AccountType, on_delete=models.PROTECT, related_name='accounts')
     is_contra_account = models.BooleanField(default=False, blank=True)
+    is_cash_account = models.BooleanField(default=False, blank=True)
 
     def __str_(self):
         return self.title
+
+
+class Journal(CompanyFieldMixin, TimeStampedModel, models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    series = models.SmallIntegerField(blank=True)
+    code = models.CharField(max_length=50, blank=True)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.code
+
+
+class JournalItem(TimeStampedModel, models.Model):
+    journal = models.ForeignKey(
+        Journal, on_delete=models.PROTECT, related_name='items')
+    account = models.ForeignKey(
+        Account, on_delete=models.PROTECT, related_name='journals')
+    debit = models.DecimalField(max_digits=14, decimal_places=2)
+    credit = models.DecimalField(max_digits=14, decimal_places=2)
+    source_type = models.CharField(max_length=20)
+    source_reference = models.CharField(max_length=200)
