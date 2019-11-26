ec3d642920456b5372ed8065ddfb1da12d875c37
hanz <hctura.official@gmail.com>
2019-11-26 11:57:01 +0800

    detail, view, edit pages

--- config/urls.py
+++ config/urls.py
@@ -21,12 +21,13 @@ from django.urls import include, path
 from debug_toolbar import urls as debug_toolbar_urls
 
 urlpatterns = [
+    path('accounting/', include('accounting.urls')),
     path('contacts/', include('contacts.urls')),
     path('', include('system.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    urlpatterns += [
+    urlpatterns = [
         path('__debug__/', include(debug_toolbar_urls))
-    ]
+    ] + urlpatterns
